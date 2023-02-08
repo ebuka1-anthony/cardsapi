7e5db000757201626cf44ad6678e8903fb782585
ebuka anthony <106475290+ebuka1-anthony@users.noreply.github.com>
2023-02-08 09:57:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+//Example fetch using pokemonapi.co
+
+
+
+
+let deckId = ''     
+//document.querySelector('button').addEventListener('click', drawTwo)
+//
+//function drawTwo(){
+// const choice = document.querySelector('input').value
+// console.log(choice)
+// const url = 'https://pokeapi.co/a'+choice
+  
+
+  fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        deckId = data.deck_Id
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
+      document.querySelector('button').addEventListener('click', drawTwo)
+
+      function drawTwo(){
+         
+        const url = 'https://www.deckofcardsapi.com/api/deck/new/draw/?count=2'
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('#player1').src = data.cards[0].image
+            document.querySelector('#player2').src = data.cards[1].image
+
+            let player1Val = convertToNum(data.cards[0].value)
+            let player2Val = convertToNum(data.cards[1].value)
+            if(player1Val > player2Val){
+              document.querySelector('h3').innerText = 'Player1 Wins'
+            }else if(player1Val <  player2Val){
+              document.querySelector('h3').innerText = 'Player2 Wins'
+            }else{
+              document.querySelector('h3').innerText = 'Time For WAR'
+            }
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+      }
+
+     function convertToNum(val){
+              if(val === 'ACE'){
+                    return 14    
+              }else if(val === 'KING'){
+                return 13
+              }else if(val === 'QUEEN'){
+                return 12
+              }else if(val === 'JACK'){
+                return 11
+              }else{
+                  return Number(val)
+              }
+     }
+
+     
+
+      //https://pokeapi.co/api/v2/pokemon/
+//img/pokemon.jpeg
